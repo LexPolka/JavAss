f57eeb70efc61dff059e8d0cc204749c4c12eaca
admin <admin@DESKTOP-7FIIS1P>
2023-09-25 23:31:18 +0800

    2nd commit!

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -1,3 +1,4 @@
+//AMOGUS SUSSY 11:30pm
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
